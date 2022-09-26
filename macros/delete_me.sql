baf82d8b3a1a6bdb47ca03d62954ed6fb4c18501
Dave Connors <dave.connors@fishtownanalytics.com>
2022-09-26 13:19:42 +0000

    hello

--- macros/delete_me.sql
+++ macros/delete_me.sql
@@ -1,5 +1,7 @@
 {% macro delete_me(text) %}
     {% if text %}
         delete this text: {{ text }} !! or ELSE!
+    {% else %}
+        have a nice day {{ target.user }}!
     {% endif %}
 {% endmacro %}
