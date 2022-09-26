7f249d30411ba980fc7c5d3702ed560b7d5a3348
Dave Connors <dave.connors@fishtownanalytics.com>
2022-09-26 13:52:30 -0500

    remove bad join

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -42,21 +42,17 @@ order_items_summary as (
 
         sum(case when products.is_food_item = 1 then price else 0 end) as subtotal_drink_items,
         sum(case when products.is_drink_item = 1 then price else 0 end) as subtotal_food_items,
-        sum(price) as subtotal,
-
-        sum(supplies.supply_cost) as order_cost 
+        sum(price) as subtotal
 
     from order_items
     inner join products 
         on order_items.product_id = products.product_id
-    inner join supplies
-        on products.product_id = supplies.product_id
 
     group by 1
 
 ),
 
-{# order_supplies_summary as (
+order_supplies_summary as (
 
     select
         order_id,
@@ -69,7 +65,7 @@ order_items_summary as (
     
     group by 1
 
-), #}
+),
 
 joined as (
 
@@ -84,7 +80,7 @@ joined as (
         order_items_summary.subtotal_food_items,
         order_items_summary.subtotal,
 
-        order_items_summary.order_cost,
+        order_supplies_summary.order_cost,
 
         -- rank this order for the customer
         row_number() over (
@@ -99,8 +95,8 @@ joined as (
         on orders.order_id = order_items_summary.order_id
     inner join locations 
         on orders.location_id = locations.location_id
---    inner join order_supplies_summary
---        on orders.order_id = order_supplies_summary.order_id
+    inner join order_supplies_summary
+        on orders.order_id = order_supplies_summary.order_id
 
 ),
 
