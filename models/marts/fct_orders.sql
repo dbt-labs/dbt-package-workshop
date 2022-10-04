a701b132796f16b41f3898eeb356eb583cbe4c69
Elize Papineau <elize.papineau@dbtlabs.com>
2022-10-04 13:51:29 -0700

    Remove second join example

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -56,21 +56,6 @@ order_items_summary as (
 
 ),
 
-{# order_supplies_summary as (
-
-    select
-        order_id,
-    
-        sum(supplies.supply_cost) as order_cost
-
-    from order_items
-    inner join supplies 
-        on supplies.product_id = order_items.product_id
-    
-    group by 1
-
-), #}
-
 joined as (
 
     select
@@ -99,8 +84,6 @@ joined as (
         on orders.order_id = order_items_summary.order_id
     inner join locations 
         on orders.location_id = locations.location_id
---    inner join order_supplies_summary
---        on orders.order_id = order_supplies_summary.order_id
 
 ),
 
